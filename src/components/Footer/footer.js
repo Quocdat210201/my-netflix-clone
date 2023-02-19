c18f77cb46f574ad6c9d16a857451d40cecf57c9
Quocdat210201 <pqd2102@gmail.com>
2023-02-19 21:57:32 +0700

    update footer 19/2

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,7 +1,67 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa'
+
+const footerLinks = [
+    "Audio Description",
+    " Help Center",
+    "Gift Cards",
+    "Media Center",
+    "Investor Relations",
+    "Jobs", "Term of Use",
+    "Privacy",
+    "Legal Notices",
+    "Cookie Preferences",
+    "Corporate Information",
+    "Contact Us",
+]
+
 function Footer() {
-    return <div>
-        Copyright © 2022 Netflix by Phan Quoc Dat
-    </div>;
+    return (
+        <div className="pb-2 pt-24 bg-[var(--primary-color)] px-10">
+            <div className="max-w-[980px] mx-auto">
+                <div className="flex items-center text-[#808080] mb-4">
+                    <Tippy content="Facebook" placement="top" theme="light">
+                        <div className="cursor-pointer mx-2 text-[24px] flex items-center justify-center w-[42px] h-[42px] rounded-full social-media">
+                            <FaFacebookF />
+                        </div>
+                    </Tippy>
+                    <Tippy content="Instagram" placement="top" theme="light">
+                        <div className="cursor-pointer mx-2 text-[24px] flex items-center justify-center w-[42px] h-[42px] rounded-full social-media">
+                            <FaInstagram />
+                        </div>
+                    </Tippy>
+                    <Tippy content="Twitter" placement="top" theme="light">
+                        <div className="cursor-pointer mx-2 text-[24px] flex items-center justify-center w-[42px] h-[42px] rounded-full social-media">
+                            <FaTwitter />
+                        </div>
+                    </Tippy>
+                    <Tippy content="Youtube" placement="top" theme="light">
+                        <div className="cursor-pointer mx-2 text-[24px] flex items-center justify-center w-[42px] h-[42px] rounded-full social-media">
+                            <FaYoutube />
+                        </div>
+                    </Tippy>
+                </div>
+                <div>
+                    <ul className="flex items-start flex-row flex-wrap px-3.5">
+                        {
+                            footerLinks.map((item, index) => (
+                                <li
+                                    className="text-[13px] text-[#808080] list-none flex-[25%] mb-4 hover:text-[var(--white-color)] hover:font-medium transition"
+                                    key={index}>
+                                    <a href="" alt="">
+                                        <span>{item}</span>
+                                    </a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="text-[13px] text-[#808080] mt-2">Copyright © 2022 Netflix by Phan Quoc Dat</div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
--- src/components/MovieDetail/movieDetail.js
+++ src/components/MovieDetail/movieDetail.js
@@ -3,6 +3,8 @@ import { FaPlay } from "react-icons/fa";
 import { BsPlus } from "react-icons/bs";
 import { AiTwotoneLike, AiOutlineLike } from "react-icons/ai"
 import { useState } from 'react';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 import { useDispatch } from 'react-redux';
 import { setMovieDetail } from '../store/action';
 import Moment from 'react-moment';
@@ -30,33 +32,37 @@ function MoviesDetail(props) {
                     <img
                         src={movie ?
                             (`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`
-                            || `https://image.tmdb.org/t/p/original/${movie.poster_path}`) : 'Error'} alt="Thumbnails"
+                                || `https://image.tmdb.org/t/p/original/${movie.poster_path}`) : 'Error'} alt="Thumbnails"
                         className='rounded-t-md object-cover w-full h-full' />
                 </div>
-                <div className="absolute top-[260px] left-[60px] text-[var(--white-color)]">
-                    <div className='mb-5 text-[3rem] font-semibold font-serif'>
-                        <h1>{movie ? (movie.name || movie.title) : ''}</h1>
-                    </div>
+                <div className="absolute top-[360px] left-[60px] text-[var(--white-color)]">
                     <div className="flex items-center">
                         <button className="flex bg-[#fff] text-[#000] btn hover:bg-[#e6e6e6]">
                             <FaPlay className="w-7 h-7 mr-4" />
                             Play
                         </button>
-                        <div className="w-10 h-10 bg-[rgba(42,42,42,0.6)] border-2 border-[hsla(0,0%,100%,0.5)] text-[#fff] text-[2rem] flex items-center justify-center rounded-full cursor-pointer hover:border-[#fff] transition mr-2.5">
-                            <BsPlus />
-                        </div>
-                        <div className="w-10 h-10 bg-[rgba(42,42,42,0.6)] border-2 border-[hsla(0,0%,100%,0.5)] text-[#fff] text-[1.4rem] flex items-center justify-center rounded-full cursor-pointer hover:border-[#fff] transition mr-2.5"
-                            onClick={() => setVisible(prev => !prev)}
-                        >
-                            {
-                                visible ? <AiOutlineLike /> : <AiTwotoneLike />
-                            }
-                        </div>
+                        <Tippy content="Add to My List" placement="top" theme="light">
+                            <div className="w-10 h-10 bg-[rgba(42,42,42,0.6)] border-2 border-[hsla(0,0%,100%,0.5)] text-[#fff] text-[2rem] flex items-center justify-center rounded-full cursor-pointer hover:border-[#fff] transition mr-2.5">
+                                <BsPlus />
+                            </div>
+                        </Tippy>
+                        <Tippy content="Like" placement="top" theme="light">
+                            <div className="w-10 h-10 bg-[rgba(42,42,42,0.6)] border-2 border-[hsla(0,0%,100%,0.5)] text-[#fff] text-[1.4rem] flex items-center justify-center rounded-full cursor-pointer hover:border-[#fff] transition mr-2.5"
+                                onClick={() => setVisible(prev => !prev)}
+                            >
+                                {
+                                    visible ? <AiOutlineLike /> : <AiTwotoneLike />
+                                }
+                            </div>
+                        </Tippy>
                     </div>
                 </div>
-                <div className="text-[#fff] flex justify-between absolute py-2 px-14">
-                    <div className="w-full max-w-[28rem]">
-                        <div className="mb-8">
+                <div className='text-[2rem] font-semibold font-serif text-[#fff] px-14 pb-1.5' >
+                    <h1 className=''>{movie ? (movie.name || movie.title) : ''}</h1>
+                </div>
+                <div className="text-[#fff] flex justify-between absolute pb-2 px-14">
+                    <div className="w-full max-w-[28rem]" >
+                        <div className="mb-2.5">
                             <span className="mr-1 text-[#46d369] text-[16px] font-semibold">96% Match </span>
                             <span className="mr-1"> 2020 </span>
                             <span className="mr-1 maturity"> 16+ </span>
@@ -66,8 +72,8 @@ function MoviesDetail(props) {
                     </div>
                     <div className="pl-12 text-[14px]">
                         <div className="flex items-center leading-7">
-                            <h1 className="mr-2 text-[#777]">Rating: </h1>
-                            <span className="italic">{movie ? movie.vote_average * 10 : 'Error'} %</span>
+                            <h1 className="mr-2 text-[#777]">Rating:</h1>
+                            <span className="italic">{movie ? movie.vote_average * 10 : 'Error'}%</span>
                         </div>
                         <div className="flex items-center leading-7">
                             <h1 className="mr-2 text-[#777]">Popularity: </h1>
